9ce433221c9be9747e953290125b00f8584358b2
dannyaw <feng73440@gmail.com>
2022-07-27 19:17:23 +0800

    Clean code

--- eth_holder/lib.rs
+++ eth_holder/lib.rs
@@ -4,7 +4,6 @@
 use fat_utils::attestation;
 use ink_env::AccountId;
 use ink_lang as ink;
-use ink_prelude::{string::String, vec::Vec};
 use pink_extension as pink;
 
 #[pink::contract(env=PinkEnvironment)]
@@ -32,7 +31,7 @@ mod eth_holder {
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub struct EthHolder {
     	secret_key: String,
-    	//public_key: String,
+    	public_key: String,
     }
 
     /// Errors that can occur upon calling this contract.
@@ -74,10 +73,18 @@ mod eth_holder {
     }
 
     fn generate_account() -> EthHolder {
-        let random_bytes = pink::ext().getrandom(32);
-        let secret = SecretKey::from_slice(&random_bytes).expect("32 bytes, within curve order");
-        /*let secp = Secp256k1::new();
-        let public = PublicKey::from_secret_key(&secp, &secret);
+        let sec_key = SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order");
+        let pub_key = PublicKey::from_slice(&[
+    0x02,
+    0xc6, 0x6e, 0x7d, 0x89, 0x66, 0xb5, 0xc5, 0x55,
+    0xaf, 0x58, 0x05, 0x98, 0x9d, 0xa9, 0xfb, 0xf8,
+    0xdb, 0x95, 0xe1, 0x56, 0x31, 0xce, 0x35, 0x8c,
+    0x3a, 0x17, 0x10, 0xc9, 0x62, 0x67, 0x90, 0x63,
+]).expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
+        /*let random_bytes = pink::ext().getrandom(32);
+        let secretKey = SecretKey::from_slice(&random_bytes).expect("32 bytes, within curve order");
+        let secp = Secp256k1::new();
+        let publicKey = PublicKey::from_secret_key(&secp, &secret);
         let public_key_encode= public.serialize_uncompressed();
         debug_assert_eq!(public_key_encode[0], 0x04);
         let hash = keccak256(&public_key_encode[1..]);
@@ -85,8 +92,8 @@ mod eth_holder {
 */
 
         EthHolder {
-            secret_key: format!("{}", secret.to_string()),
-            //public_key: public.to_string(),
+            secret_key: format!("{}", sec_key.to_string()),
+            public_key: pub_key.to_string(),
         }
     }
 
@@ -102,7 +109,8 @@ mod eth_holder {
 
         #[ink::test]
         fn get_address() {
-           // generate_account();
+           let account = generate_account();
+           println!("secret key: {:?}", account);
 	}
     }
 }
